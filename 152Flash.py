93a0a16f38d9b4c5991161b4e6c6987fbf98f75f
Chris Watkins <35972776+CJWatkins9@users.noreply.github.com>
2019-10-22 14:27:24 -0400

    Polishing
    Fixed bugs in menus 1 and 2

--- 152Flash.py
+++ 152Flash.py
@@ -3,7 +3,7 @@ import random
 terms = {
 "55 KIAS":"Vx - (Best Angle of Climb)",
 "67 KIAS": "Vy - (Best Rate of Climb)",
-"111 KIAS": "Vno - (Normal Operating Speed",
+"111 KIAS": "Vno - (Normal Operating Speed)",
 "149 KIAS": "Vne - (Never exceed speed)",
 "98 KIAS": "Va - (Manuevering speed)",
 "40 KIAS": "Vs - (Stall speed)",
@@ -19,11 +19,9 @@ def main():
         menu = input("\t\tEnter menu option: ")
         if menu == "1":
             print("\n")
-            term, definition = random.choice(list(terms.items()))
-            for term in terms:
-                print(term + ":" + definition)
+            for term, definition in terms.items():
+                print(term, definition)
             input("\n\tPress Enter to return to Main Menu. \n")
-        
         elif menu == "2":
             print("\n\t\tType 'Exit' to return to main Menu \n")
             def generate_question():
@@ -33,13 +31,13 @@ def main():
             term = generate_question()
             guess = None
             while guess != 'exit':
-                guess = input("\tWhat is the speed? ").lower()
+                guess = input("\tWhat is the speed? ").upper()
                 if guess == term:
                     print('Correct!')
                     if input("\tAnother definition? (y/n)").lower() in ["y", "yes"]:
                         term = generate_question()
                     else:
-                        input("\n\tPress Enter to return to Main Menu. \n")
+                        break
                 else:
                     print('\tThat is not correct.')
         elif menu == "3":
